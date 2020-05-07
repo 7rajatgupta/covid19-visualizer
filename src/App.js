d7e711db68b14b329ede199a7dad798bba7bb4c4
Rajat Gupta <7rajatgupta@gmail.com>
2020-05-07 16:53:39 +0530

    cases-card-complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api";
 import styles from "./App.module.css";
 
-function App() {
+const App = () => {
+  const [basicInfo, setBasicInfo] = useState({});
+  const getCovidData = async () => {
+    const basicData = await fetchData();
+    setBasicInfo(basicData);
+  };
+
+  useEffect(() => {
+    getCovidData();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Cards />
+      <Cards basicData={basicInfo} />
       <CountryPicker />
       <Chart />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,118 @@
 import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
+import cx from "classnames";
+import styles from "./Cards.module.css";
 
-const Cards = () => {
-  return <div>Cards</div>;
+const Cards = ({ basicData }) => {
+  console.log(basicData);
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.infected)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">
+              {basicData.confirmed === undefined ? (
+                "Updating..."
+              ) : (
+                <CountUp
+                  start={0}
+                  end={basicData.confirmed.value}
+                  duration={7}
+                  separator=","
+                />
+              )}
+            </Typography>
+            <Typography color="textSecondary">
+              {basicData.lastUpdate === undefined
+                ? "Updating..."
+                : new Date(basicData.lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">
+              Num of active cases of Chinese Virus
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.recovered)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Patients Recovered
+            </Typography>
+            <Typography variant="h5">
+              {basicData.recovered === undefined ? (
+                "Updating..."
+              ) : (
+                <CountUp
+                  start={0}
+                  end={basicData.recovered.value}
+                  duration={5}
+                  separator=","
+                />
+              )}
+            </Typography>
+            <Typography color="textSecondary">
+              {basicData.lastUpdate === undefined
+                ? "Updating..."
+                : new Date(basicData.lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">
+              Num of recoveries from Chinese Virus
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.deaths)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">
+              {basicData.deaths === undefined ? (
+                "Updating..."
+              ) : (
+                <CountUp
+                  start={0}
+                  end={basicData.deaths.value}
+                  duration={4}
+                  separator=","
+                />
+              )}
+            </Typography>
+            <Typography color="textSecondary">
+              {basicData.lastUpdate === undefined
+                ? "Updating..."
+                : new Date(basicData.lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">
+              Total number of deaths from Chinese Virus.
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Cards;
